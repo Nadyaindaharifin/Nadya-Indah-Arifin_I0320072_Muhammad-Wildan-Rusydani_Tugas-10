395566747d890d55c0cf680045f0bb2fb4de91d0
Nadya Indah Arifin <nandyaindah8@gmail.com>
2021-05-04 12:52:41 +0700

    Tugas 10

---
+++ soal 1.py
@@ -0,0 +1,10 @@
+#nama file: mkdir.py
+
+import os
+
+def main():
+    os.mkdir("biodatadiri")
+
+ifname = "main"
+
+main()
---
+++ soal 4.py
@@ -0,0 +1,10 @@
+#Nama_File: mkdir.py
+
+import os
+
+def main():
+    os.rename("biodatadiri","Nadya Indah Arifin_I0320072")
+
+ifname = "main"
+
+main()
